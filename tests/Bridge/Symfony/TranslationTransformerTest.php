feef7ae79dbc2ae9e6442f0347cc903c77534ecd
Milan Radovic <radovicmmilan@gmail.com>
2026-01-16 13:28:18 +0100

    Add test case for combining Translate and Enum transformers

--- tests/Bridge/Symfony/TranslationTransformerTest.php
+++ tests/Bridge/Symfony/TranslationTransformerTest.php
@@ -3,9 +3,11 @@ declare(strict_types=1);
 
 namespace ControlBit\Dto\Tests\Bridge\Symfony;
 
+use ControlBit\Dto\Attribute\Transformers\Enum;
 use ControlBit\Dto\Attribute\Transformers\Translate;
 use ControlBit\Dto\Contract\Mapper\MapperInterface;
 use ControlBit\Dto\Tests\Bridge\Symfony\App\Helper\StringModifier;
+use ControlBit\Dto\Tests\Resources\SimpleEnum;
 use ControlBit\Dto\Tests\SymfonyTestCase;
 
 class TranslationTransformerTest extends SymfonyTestCase
@@ -56,5 +58,22 @@ class TranslationTransformerTest extends SymfonyTestCase
         $mappedObject = self::getContainer()->get(MapperInterface::class)->map($from, $to);
 
         $this->assertEquals('FOO', $mappedObject->foo);
+    }
+
+    public function testTranslateInCombinationWithEnumTransformer(): void
+    {
+        $from = new class() {
+            public SimpleEnum $foo = SimpleEnum::FOO;
+        };
+
+        $to = new class() {
+            #[Enum(SimpleEnum::class)]
+            #[Translate]
+            public string $foo;
+        };
+
+        $mappedObject = self::getContainer()->get(MapperInterface::class)->map($from, $to);
+
+        $this->assertEquals('Just Foo', $mappedObject->foo);
     }
 }
