b6f417807ffc560c58c5c64f3337082cea4cc9a3
Milan Radovic <radovicmmilan@gmail.com>
2026-01-18 18:41:57 +0100

    Support for object to DTO value converter.

--- src/Mapper/ValueConverter/DtoToDto.php
+++ src/Mapper/ValueConverter/DtoToDto.php
@@ -3,22 +3,36 @@ declare(strict_types=1);
 
 namespace ControlBit\Dto\Mapper\ValueConverter;
 
+use ControlBit\Dto\Attribute\Dto;
 use ControlBit\Dto\Contract\Accessor\SetterInterface;
+use ControlBit\Dto\Contract\Mapper\MapperInterface;
 use ControlBit\Dto\Contract\Mapper\ValueConverterInterface;
 use ControlBit\Dto\Mapper\Mapper;
 
 /**
- * Usual case when we have Array on one side, and we need to map to object props on another
+ * Case when we have some object on the source property side, and we need to convert it/map it to DTO.
  */
-final class ArrayToObject implements ValueConverterInterface
+final class DtoToDto implements ValueConverterInterface
 {
+    public function __construct(private readonly MapperInterface $mapper)
+    {
+    }
+
     public function supports(SetterInterface $setter, mixed $value): bool
     {
-        if (\is_object($value) || !\is_iterable($value)) {
+        if (!\is_object($value)) {
             return false;
         }
 
-        return $setter->getType()->hasOnlyOneClass();
+        if (!$setter->getType()->hasOnlyOneClass()) {
+            return false;
+        }
+
+        if (!$setter->getAttributes()->has(Dto::class)) {
+            return false;
+        }
+
+        return true;
     }
 
     /**
